18516046d5891552a129a63bd06644d1ea8363c7
Raunak Mandal <iamriju2000@gmail.com>
2021-05-24 19:53:09 +0530

    Tweaked Homepage

---
+++ script.js
@@ -0,0 +1,17 @@
+// Countdown
+var finalDate = new Date("Jun 1, 2021").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = finalDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("daytext").innerHTML = days + "<br>DAYS";
+    document.getElementById("hourtext").innerHTML = hours + "<br>HOURS";
+    document.getElementById("minstext").innerHTML = minutes + "<br>MINS";
+    document.getElementById("secstext").innerHTML = seconds + "<br>SECS";
+
+
+}, 1000);
