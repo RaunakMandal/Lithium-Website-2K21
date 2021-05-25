bbc50bafb491e9c64e676b568fc84b80a6f58b92
Raunak Mandal <iamriju2000@gmail.com>
2021-05-25 17:58:13 +0530

    Added carousel and others

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// Countdown
-var finalDate = new Date("Jun 1, 2021").getTime();
-var x = setInterval(function() {
+$(document).ready(function () {
+  // Changing Text Animation
+  var type = new Typed(".changing", {
+    strings: ["Music", "Drama", "Dance", "Recitation", "Talent Hunt", "Mr and Miss Freshers"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  // Countdown
+  var finalDate = new Date("Jun 1, 2021").getTime();
+  var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = finalDate - now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
@@ -12,14 +22,5 @@ var x = setInterval(function() {
     document.getElementById("hourtext").innerHTML = hours + "<br>HOURS";
     document.getElementById("minstext").innerHTML = minutes + "<br>MINS";
     document.getElementById("secstext").innerHTML = seconds + "<br>SECS";
-}, 1000);
-
-// Changing Text
-var typing = () => {
-    var type = new Typed(".changing", {
-        strings: ["Dance", "Drama", "Mr and Ms Fresher", "Singing"],
-        typeSpeed: 100,
-        backSpeed: 80,
-        loop: true
-    });
-};
+  }, 1000);
+});
