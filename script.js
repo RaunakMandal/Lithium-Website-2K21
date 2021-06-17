e3ba9ff026c1357a03f9ac54041837bfbb411c38
kabirulhassan <kabirulhassan7@gmail.com>
2021-06-18 05:16:08 +0530

    Added na

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  let elementsArray = document.querySelectorAll(".nav-links li, .burger");
+
+  elementsArray.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      //toggle navbar
+      nav.classList.toggle("nav-active");
+
+      //animate links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`;
+        }
+      });
+      //burger animation
+      burger.classList.toggle("toggle");
+    });
+  });
+};
+
+
 $(document).ready(function () {
+
+  navSlide();
   // Changing Text Animation
   var type = new Typed(".changing", {
     strings: [
